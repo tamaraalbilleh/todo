fcedd64495dc5f4e5d2dadc49a1a42c49bcb3595
tamaraalbilleh <tamara2albella@gmail.com>
2021-07-03 18:59:06 +0300

    done with lab 34

--- src/app.js
+++ src/app.js
@@ -10,9 +10,7 @@ export default function App () {
       <>
         <Header/>
         <If condition={context.loggedIn}>
-        
           <ToDo />
-         
         </If>
         
       </>
